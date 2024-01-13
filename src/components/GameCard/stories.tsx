e210fea6393f250e7a77981934c82e66fdaaf50c
Felipe Alves <profelipealves@outlook.com>
2024-01-13 16:49:49 -0300

    add components GameCard

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import GameCard from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard
+} as Meta
+
+export const Default: StoryObj = {}
