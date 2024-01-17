e4e3cf4b7d219d12321cbe8f4ed54b8503208954
Felipe Alves <profelipealves@outlook.com>
2024-01-16 21:28:40 -0300

    added type game card

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,6 +1,13 @@
 import * as S from './styles'
 
-const GameCard = () => (
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+}
+
+const GameCard = ({title, developer, img, price}: GameCardProps) => (
   <S.Wrapper>
     <h1>GameCard</h1>
   </S.Wrapper>
