2b1eb990250f9b1e84ea87bc2ea361c1a056339a
Felipe Alves <profelipealves@outlook.com>
2024-03-13 05:31:09 -0300

    test(slider): added tests e config tests for slick

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     '!src/styles/**'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
