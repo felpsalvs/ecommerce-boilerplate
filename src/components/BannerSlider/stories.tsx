08c661831cde599c63fa7413b0cde840e4a599ae
felpsalvs <profelipealves@outlook.com>
2024-03-21 10:47:10 -0300

    chore: added BannerSlider

---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import BannerSlider from '.'
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider
+} as Meta
+
+export const Default: StoryObj = {}
