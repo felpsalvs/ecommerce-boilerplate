562420f80b2ed7b7c5fd5e3ca09e99478d6d8ca1
Felipe Alves <profelipealves@outlook.com>
2024-05-20 17:55:06 -0300

    feat: added component GameCardSlider

---
+++ src/components/GameCardSlider/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const GameCardSlider = () => (
+  <S.Wrapper>
+    <h1>GameCardSlider</h1>
+  </S.Wrapper>
+)
+
+export default GameCardSlider
---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import GameCardSlider from '.'
+
+export default {
+  title: 'GameCardSlider',
+  component: GameCardSlider
+} as Meta
+
+export const Default: StoryObj = {}
